9b5e5b5b8300ba24819dab3047a6d8bc40ab5a96
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2022-04-19 11:11:49 -0500

    Init Commit

--- App.tsx
+++ App.tsx
@@ -13,10 +13,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme}/>
+          <StatusBar/>
+        </SafeAreaProvider>
     );
   }
 }
